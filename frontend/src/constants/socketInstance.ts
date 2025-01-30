f3d3cab81c4ad622f4914a6fcadd22916255b47a
melodylatency <melodylatency@yahoo.com>
2025-01-30 16:34:29 +0500

    potential

--- frontend/src/constants/socketInstance.ts
+++ frontend/src/constants/socketInstance.ts
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 const socket = io("wss://project.danielsaynov.com", {
-  path: "/api/socket.io/", // ✅ Fix: Ensure WebSockets go to /api/socket.io/
-  transports: ["websocket"], // ✅ Use WebSockets instead of polling
-  withCredentials: true, // ✅ Keep session handling
+  path: "/api/socket.io/",
+  transports: ["websocket"],
+  withCredentials: true,
 });
 
 export { socket };
